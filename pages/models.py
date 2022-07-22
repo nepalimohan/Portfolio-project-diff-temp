172af700d771072123233f471789080e30287509
nepalimohan <nepalimohan@outlook.com>
2022-07-22 13:29:26 +0545

    ModelsCreated

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,31 @@
+from random import choices
+from secrets import choice
 from django.db import models
+from multiselectfield import MultiSelectField
+from ckeditor.fields import RichTextField
+
+skills_choices = (
+    ('Python', 'Python'),
+    ('UI/UX', 'UI/UX'),
+    ('HTML/CSS', 'HTML/CSS'),
+    ('Postgres', 'Postgres'),
+)
 
 # Create your models here.
+class Detail(models.Model):
+    name = models.CharField(max_length=100)
+    skills = MultiSelectField(choices=skills_choices)
+    what_i_do = RichTextField()
+    about = RichTextField()
+    facebook_link = models.URLField(max_length=100)
+    github_link = models.URLField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+class PastProject(models.Model):
+    name = models.CharField(max_length=100)
+    github_link = models.URLField(max_length=100)
+    
+    def __str__(self):
+        return self.name
