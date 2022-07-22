cb2bc94c025b4771f9d51a9866a88a6ee8c724ec
nepalimohan <nepalimohan@outlook.com>
2022-07-22 11:47:42 +0545

    PageMafeDynamic

--- static/js/script.js
+++ static/js/script.js
@@ -21,19 +21,19 @@ for (var i=0; themeDots.length > i; i++){
 
 function setTheme(mode){
 	if(mode == 'light'){
-		document.getElementById('theme-style').href = 'static/css/default.css'
+		document.getElementById('theme-style').href = 'default.css'
 	}
 
 	if(mode == 'blue'){
-		document.getElementById('theme-style').href = 'static/css/blue.css'
+		document.getElementById('theme-style').href = 'blue.css'
 	}
 
 	if(mode == 'green'){
-		document.getElementById('theme-style').href = 'static/css/green.css'
+		document.getElementById('theme-style').href = 'green.css'
 	}
 
 	if(mode == 'purple'){
-		document.getElementById('theme-style').href = 'static/css/purple.css'
+		document.getElementById('theme-style').href = 'purple.css'
 	}
 
 	localStorage.setItem('theme', mode)
