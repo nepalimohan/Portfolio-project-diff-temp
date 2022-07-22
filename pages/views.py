f104533ac889e376e910cf3bc8d6d01a4051ee2f
nepalimohan <nepalimohan@outlook.com>
2022-07-22 15:25:47 +0545

    DataAdded

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Detail, Project
 
 # Create your views here.
 def home(request):
-    return render(request, 'pages/home.html')
+    details = Detail.objects.all()
+    projects = Project.objects.all()
+    data = {
+        'details':details,
+        'projects': projects,
+    }
+    return render(request, 'pages/home.html', data)
