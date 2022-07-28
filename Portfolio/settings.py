d0861a26abd63728563653b7ea8e6761856d9754
nepalimohan <nepalimohan@outlook.com>
2022-07-28 12:01:51 +0545

    SendingMail

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -140,3 +140,12 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Email sending settings
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'rockerdeula@gmail.com'
+EMAIL_HOST_PASSWORD = 'eznymlkudhqxnwnf'
+EMAIL_USE_SSL = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Detail, Project
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
-    details = Detail.objects.all()
-    projects = Project.objects.all()
-    data = {
-        'details':details,
-        'projects': projects,
-    }
-    return render(request, 'pages/home.html', data)
+    if request.method == 'POST':
+        name = request.POST['name']
+        subject = request.POST['subject']
+        email = request.POST['email']
+        message = request.POST['message']
+        
+        message_body = 'Name: ' + name + '. Email: ' + email + '. Message' + message
+        
+        admin_info = User.objects.get(is_superuser=True)
+        admin_email = admin_info.email
+        
+        send_mail(
+                subject,
+                message_body,
+                'rockerdeula@gmail.com',
+                [admin_email],
+                fail_silently=False,
+                )
+        return redirect('home')
+        
+    else:   
+        details = Detail.objects.all()
+        projects = Project.objects.all()
+        data = {
+            'details':details,
+            'projects': projects,
+        }
+        return render(request, 'pages/home.html', data)
